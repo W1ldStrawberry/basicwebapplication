789bc5354b53b543fbeba0264d04689c992d4cf3
W1ldStrawberry <maxonchik372@yandex.ru>
2018-07-12 12:20:48 +0300

    my first initial commit created base structure of application

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 const array = [2, 3, 5, 6, 1];
 const sortedArray = array.sort((item1, item2) => item1 > item2);
-
-console.log(sortedArray);
-console.log('sorted');
+console.log('Here sorted array: ' + sortedArray.toString());
